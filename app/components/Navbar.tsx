27f3bff17fdfe9d213a52d019d981ab3f765448f
SanketLakhani <77873512+SanketLV@users.noreply.github.com>
2025-07-20 11:12:13 +0530

    Update app/components/Navbar.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Link} from "react-router/internal/react-server-client";
+-import {Link} from "react-router/internal/react-server-client";
++import {Link} from "react-router";
 
 const Navbar = () => {
     return (
